7317e0ad594793d4b2aa4280104fb0de615faabe
kirillovsky <kirillow.aleks@gmail.com>
2018-02-18 09:21:17 +0300

    task8 - fix not compile

--- src/i_introduction/_8_Smart_Casts/n08SmartCasts.kt
+++ src/i_introduction/_8_Smart_Casts/n08SmartCasts.kt
@@ -5,7 +5,7 @@ import util.doc8
 
 // 'sealed' modifier restricts the type hierarchy:
 // all the subclasses must be declared in the same file
-sealed class Expr
+open class Expr
 class Num(val value: Int) : Expr()
 class Sum(val left: Expr, val right: Expr) : Expr()
 
@@ -13,6 +13,7 @@ fun eval(e: Expr): Int =
         when (e) {
             is Num -> todoTask8(e)
             is Sum -> todoTask8(e)
+            else -> throw IllegalArgumentException()
         }
 
 fun todoTask8(expr: Expr): Nothing = TODO(
